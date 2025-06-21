4276cad76c207d3814b4fb951bd439b14ede832f
abzkrvni <a.bezkorovainyi@gmail.com>
2025-06-21 21:55:54 +0300

    Created glue ETL job to read raw csv and move them to bronze parquets with STR schema

---
+++ assessment_work/glue/etl_jobs/process_sales_raw_to_bronze.py
@@ -0,0 +1,43 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+import boto3
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+bucket_name = "ab-data-platform-data-lake-752953535939"
+raw_prefix = "raw/"
+bronze_prefix = "bronze/"
+
+s3 = boto3.client("s3")
+
+objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=raw_prefix)
+csv_files = [f"s3://{bucket_name}/{obj['Key']}" for obj in objects.get("Contents", []) if obj["Key"].endswith(".csv")]
+
+if not csv_files:
+    print("No CSV files found in raw/")
+    job.commit()
+    sys.exit(0)
+
+df_list = []
+for path in csv_files:
+    df = spark.read.option("header", "true").csv(path)
+    for col_name in df.columns:
+        df = df.withColumn(col_name, df[col_name].cast("string"))
+    df_list.append(df)
+
+final_df = df_list[0]
+for df in df_list[1:]:
+    final_df = final_df.unionByName(df, allowMissingColumns=True)
+
+final_df.write.mode("overwrite").parquet(f"s3://{bucket_name}/{bronze_prefix}")
+
+print("Data written to bronze zone.")
+job.commit()
