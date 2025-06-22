f0a4dbf890a4165f300dd2ef3bd366aeaa24adb5
abzkrvni <a.bezkorovainyi@gmail.com>
2025-06-22 18:24:45 +0300

    Fix sales table bronze path

--- assessment_work/glue/etl_jobs/process_sales_raw_to_bronze.py
+++ assessment_work/glue/etl_jobs/process_sales_raw_to_bronze.py
@@ -14,12 +14,14 @@ job.init(args['JOB_NAME'], args)
 
 bucket_name = "ab-data-platform-data-lake-752953535939"
 raw_prefix = "raw/"
-bronze_prefix = "bronze/"
+bronze_sales_path = f"s3://{bucket_name}/bronze/bronze_sales/" 
 
 s3 = boto3.client("s3")
-
 objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=raw_prefix)
-csv_files = [f"s3://{bucket_name}/{obj['Key']}" for obj in objects.get("Contents", []) if obj["Key"].endswith(".csv")]
+
+csv_files = [f"s3://{bucket_name}/{obj['Key']}" 
+             for obj in objects.get("Contents", []) 
+             if obj["Key"].endswith(".csv")]
 
 if not csv_files:
     print("No CSV files found in raw/")
@@ -37,7 +39,7 @@ final_df = df_list[0]
 for df in df_list[1:]:
     final_df = final_df.unionByName(df, allowMissingColumns=True)
 
-final_df.write.mode("overwrite").parquet(f"s3://{bucket_name}/{bronze_prefix}")
+final_df.write.mode("overwrite").parquet(bronze_sales_path)
 
-print("Data written to bronze zone.")
+print("Data written to bronze/sales/")
 job.commit()
